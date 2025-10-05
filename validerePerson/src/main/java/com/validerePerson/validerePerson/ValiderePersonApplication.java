0755144c6e41f7cc76068c4ed80bc9f31838f0ca
christoffertoft13 <chrtof13@gmail.com>
2025-10-05 12:20:12 +0200

    opprettet et nytt prosjekt for å validere en person

---
+++ validerePerson/src/main/java/com/validerePerson/validerePerson/ValiderePersonApplication.java
@@ -0,0 +1,13 @@
+package com.validerePerson.validerePerson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValiderePersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValiderePersonApplication.class, args);
+	}
+
+}
