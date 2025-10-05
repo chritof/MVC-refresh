0aeb4fb0107a3e187f52b6457bed6bbb47670f5b
christoffertoft13 <chrtof13@gmail.com>
2025-10-05 14:15:30 +0200

    nytt eksempel med PRG

---
+++ postRedirectGet/src/main/java/com/postRedirectGet/postRedirectGet/PostRedirectGetApplication.java
@@ -0,0 +1,13 @@
+package com.postRedirectGet.postRedirectGet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostRedirectGetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostRedirectGetApplication.class, args);
+	}
+
+}
---
+++ postRedirectGet/src/test/java/com/postRedirectGet/postRedirectGet/PostRedirectGetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postRedirectGet.postRedirectGet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostRedirectGetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
