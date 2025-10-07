ab080a1b7ddc91ae36c60417c5224a1cb7645e3b
christoffertoft13 <chrtof13@gmail.com>
2025-10-07 08:08:42 +0200

    lagt til en passord eksempel

---
+++ passord/src/test/java/com/passord/passord/PassordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passord.passord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
