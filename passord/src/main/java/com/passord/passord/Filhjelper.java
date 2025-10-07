c72202391f42800fe00ac32fa9a60a5c137484d7
christoffertoft13 <chrtof13@gmail.com>
2025-10-07 08:24:08 +0200

    fikset feil

---
+++ passord/src/main/java/com/passord/passord/Filhjelper.java
@@ -0,0 +1,16 @@
+package com.passord.passord;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Filhjelper {
+
+    public static List<String> lesInnPassordFraFil(String filnavn)
+            throws IOException {
+
+        return Files.lines(Paths.get(filnavn)).toList();
+    }
+
+}
