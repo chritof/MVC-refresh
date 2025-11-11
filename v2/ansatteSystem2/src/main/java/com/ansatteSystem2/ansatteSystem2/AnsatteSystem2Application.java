49713e37fd90a9bd4ea492fdaf5322833919cfe8
christoffertoft13 <chrtof13@gmail.com>
2025-11-11 19:03:12 +0100

    lagt til en ny fil under versjon 2

---
+++ v2/ansatteSystem2/src/main/java/com/ansatteSystem2/ansatteSystem2/AnsatteSystem2Application.java
@@ -0,0 +1,13 @@
+package com.ansatteSystem2.ansatteSystem2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsatteSystem2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsatteSystem2Application.class, args);
+	}
+
+}
---
+++ v2/ansatteSystem2/src/test/java/com/ansatteSystem2/ansatteSystem2/AnsatteSystem2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansatteSystem2.ansatteSystem2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsatteSystem2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
